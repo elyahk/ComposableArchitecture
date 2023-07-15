805e3eb57b03b29daa25a36a236957acf2f233ae
Eldorbek <eldorbek.nusratov.98@gmail.com>
2023-07-15 08:34:12 +0500

    Initial Commit

---
+++ ComposableArchitecture/ComposableArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableArchitectureApp.swift
+//  ComposableArchitecture
+//
+//  Created by eldorbek nusratov on 15/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ComposableArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComposableArchitecture/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ComposableArchitecture
+//
+//  Created by eldorbek nusratov on 15/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
